2f569b156d995dbf1547630d9174131d5b179535
CraigGalorDev <craigmitchellasus@gmail.com>
2022-05-17 10:44:42 +0000

    initial commit board setup

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def create_game_board(game_board):
+    print("    0 1 2 3 4 5 6 7 8")
+    print("    -----------------")
+    for row in range(9):
+        game_board.append(["-"] * 9)
+    letter = 0
+    for letter in range(8):
+        print(chr(letter + 65), end=" | ")
+        for column in range(len(game_board[letter])):
+            print(game_board[letter][column], end=" ")
+        print("| ")
+        letter += 1
+
+    print("    -----------------")
+
+if __name__ == "__main__":
+    game_board = []
+    create_game_board(game_board)
