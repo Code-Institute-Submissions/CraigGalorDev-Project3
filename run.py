d137382fb2a907e9c8e1f98200752acce2ca6463
CraigGalorDev <craigmitchellasus@gmail.com>
2022-05-25 22:44:27 +0000

    add global variables and a random number function

--- run.py
+++ run.py
@@ -1,22 +1,31 @@
 import random
 
-def check_board_size(size):
-    if int(size) in range(8, 20):
-        scaling = 0
-        print('good choice')
-        scaling = int(size)
-        print(type(scaling))
-    else:
-        print('incorrect please try again')
-    
-    return scaling
+# creating the global variables
+# the player sets the numerical value of the board size
+# this then sets the size of the playable board size to be displayed
+board_size = 0
+# player board displays were the player positioned thier ships and updated information miss and hits 
+player_board = 0
+# only the pseudo board is displayed on screen showing only updated miss and hits 
+# the com board has the ships locations
+com_board = 0
+com_pseudo_board = 0
+# in this basic version of battleships each ship equals a life 
+# when all 4 lives/ships are gone its game over
+player_lives = 4
+com_lives = 4
+# these dictionaries will hold the positions of the ships location on the board
+player_ships = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol boat': 0}
+com_ships = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol boat': 0}
 
-def create_boards(scale):
-    for size in scale:
-        size = "-"
-        print(size)
+def random_num():
+    '''
+    random number generator to  set the com ships location and for com guess
+    '''
+    num = round((random.random()) * 10, )
+    return num
 
-def ship_placement():
+#def ship_placement():
     pass
 
 #def main():
@@ -24,10 +33,6 @@ def ship_placement():
     print("Welcome to battleships\n")
     print("please select a\n   board size\nbetween 8 and 20")
 
-    board_size = input()
-    board_scale = check_board_size(board_size)
-    print(board_scale)
-    print(type(board_scale))
 
 #def test_1():
     list =[]
@@ -102,22 +107,13 @@ def ship_placement():
     print(col)
     print(col[0])
 
-
-#player_list = [["water", "water", "water", "water", "water"], ["water", "water", "water", "water", "water"], 
-#["water", "water", "water", "water", "water"], 
-#["water", "water", "water", "water", "water"],
-#["water", "water", "water", "water", "water"]]
-#print(player_list)
-#print("\n")
-#player_list[0].insert(1, "hit")
-#print(player_list)
-"""
-for x in range(0,1):
-    print("outer", end="")
-    for y in range(board):
-        player_list.append("water")
-    print(player_list)
-"""
+    """
+    for x in range(0,1):
+        print("outer", end="")
+        for y in range(board):
+            player_list.append("water")
+        print(player_list)
+    """
 #print("\n put in strike co-ordinates 0-4")
 #strike1 = input()
 #strike2 = input()
@@ -135,21 +131,27 @@ for x in range(0,1):
 #num = 3
 #for x in range(0, num):
     #print("hello")
-def set_board_size():
+#def set_board_size():
     list = []
-    print("\nSelect a board size: A number between 5-14")
-    count = int(input())
-    print(count)
-    if count in range(5, 15):
-        print("good choice")
-        for num in range(count):
-            list.append([])
-    else:
-        print("\nsorry that is an incorrect value")
-    print(list)
+    board_set = True
+    while board_set:
+        print("\nSelect a board size: A number between 5-14")
+        count = int(input())
+        print(count)
+        if count in range(5, 15):
+            print("good choice")
+            for num in range(count):
+                list.append([])
+                board_set = False
+        else:
+            print("\nsorry that is an incorrect value")
+            print("you need to pick a number")
+        print(list)
+            
+   
     
     print(list, end="\n")
-    water = "water"
+    water = "--"
     for x in range(len(list)):
         for y in range(len(list)):
             list[x].append(water)
@@ -164,29 +166,29 @@ def set_board_size():
     for x in range(len(list)):
         print(list[x], end="\n")
 
-def set_ships():
+#def set_ships():
     '''
     this is the conditional so that ships do not occupy the same space
     '''
-    player = {'battleship': 0 , 'destroyer': 0}
-    player['battleship'] = [[2], [4]]
-    print(player)
-    print(player['battleship'][0])
+    
+    player_ships['battleship'] = [[2], [4]]
+    print(player_ships)
+    print(player_ships['battleship'][0])
     print("\n")
-    player['destroyer'] = [[3], [4]]
-    print(player)
-    print(player['destroyer'][0])
-    if player['battleship'] == player['destroyer']:
+    player_ships['destroyer'] = [[3], [4]]
+    print(player_ships)
+    print(player_ships['destroyer'][0])
+    if player_ships['battleship'] == player_ships['destroyer']:
         print("they are the same")
     else:
         print("its all good")
-
+    '''
     player = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol boat': 0}
     com = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol boat': 0}
     print(player)
     print("\n")
     print("To set your ship positions choose a column and then a row")
-    '''
+    
     print(f"set battleship position")
     print("select a column")
     col = input()
@@ -200,7 +202,7 @@ def set_ships():
     '''
     # now to make it quicker and set the ships
     last_ship_selection = 0
-    for key, value in player.items():
+    for key, value in player_ships.items():
         print("\n" + f"set {key} position")
         print(last_ship_selection)
         print("\nselect a column")
@@ -212,24 +214,17 @@ def set_ships():
             print("please try again")
         else:
             last_ship_selection = combo
-            player[key] = combo
-    print(player)
+            player_ships[key] = combo
+    print(player_ships)
 
 
-def main():
-    set_board_size()
-    set_player_ships()
-    set_com_ships()
-    display_boards()
-    guess_position()
-    check_lives()
-    
+#def main():
+    #set_board_size()
+   # set_ships()
+   # display_boards()
+   # guess_position()
+   #3
+   # check_lives()
     
 #main()
 #set_ships()
-
-#random numbers one for loop to set the com ships and another for com guess
-col_random_num =0
-row_random_num =0
-for i in range(10):
-    print(round((random.random()) * 10, ))
