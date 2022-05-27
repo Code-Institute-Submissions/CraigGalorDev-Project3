ff85acde81a32eb36b7cee6504cb127de3842ce0
CraigGalorDev <craigmitchellasus@gmail.com>
2022-05-27 08:22:41 +0000

    finished project

--- README.md
+++ README.md
@@ -1,31 +1,121 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
-
-Welcome CraigGalorDev,
-
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
-
-## Reminders
-
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
-
-## Creating the Heroku app
-
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
-
-1. `heroku/python`
-2. `heroku/nodejs`
-
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
-
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
-
-Connect your GitHub repository and deploy as normal.
-
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
 
 -----
-Happy coding!
+# BATTLESHIPS
+
+this is a Python generated game which is played through the terminal and is deployed using a Heroku mock terminal.
+
+For those who never played BATTLESHIPS before, The object of the game is to take turns at 
+guessing were your opponets ships are positioned on a numbered grid.
+
+there are four ships, each player has the battleship, destroyer, submarine and patrol boat
+each ship represents a life and one position on the grid, making four lives in total for each player.
+
+who ever can destroy all of thier opponets ships wins the game.
+
+Here is the live version of my project
+image
+
+## How to play
+The terminal displays a welcoming intro with a quick guide to how each game element
+is visually represented on the terminal stating that 
+
+       a ship = @ symbol       a miss = O symbol         a hit = X symbol
+![welcome](assets/images/welcome.png)
+
+* Requests the users input to set the grid size in a particular range    5-14 in length
+if the selection is under or over this range then they are informed of the error and asked to
+enter another value. 
+
+* Once the board size is set the option of displaying either the player or computers board
+is availiable with a third option to skip and continue to the next section of the game.
+
+* The player is asked to input each of their ships location on to the custom sized grid
+while the computers is randomly generated.
+
+* The game starts with the opponents taking turns at guessing each others ship locations 
+
+* Eventually when either of the combatants ships/lives are lost the game is over with
+the player declared the winner or loser.
+
+## Features
+
+### Current Features
+
+* **Customized board generation**
+
+  users input sets the grid size in a particular range 5-14 in length
+
+  ![size5](assets/images/board_size5.png)![size8](assets/images/board_size8.png)
+
+  board changes to size as long as its in the range offered
+  otherwise exception handaling repeats input offer until satisfied 
+
+image
+
+* **Board display options**
+
+![display](assets/images/display_op.png)
+
+  there are three options offered to the player to either display either
+  the player or computers board or skip onto the next section of the game.
+  image
+
+* **Picking ship locations**
+
+![selection](assets/images/selection.png)
+
+  column and row inputs are requested from the player to set the location 
+  of each ship to be placed on the players board while the computers selection
+  is asigned but hidden from sight.
+
+  validation of current and other ships position in the player object is checked
+  as not to make the mistake of placing two or more ships in the same square.
+
+  co-ordinates must be inside of the board and be integrs to continue
+
+  ![ship_data](assets/images/ship_data.png)
+
+  Data of ships positions is contained in the player_ships object
+  and com_ships object
+
+## Future features
+   ships of varying sizes can occupy more than one square position on the grid
+
+
+## Data Model
+*  Lists and Dictionaries
+
+    Lists store the populated grid icon strings based
+    on board size input and updated as the game carries on
+
+    Dictionares store the cloumn and row values of each ship location
+
+
+
+## Bugs
+
+ * solved bugs
+
+
+## Remaining bugs
+ * no remaining bugs
+
+## Validator Testing
+* PEP8
+ some Errors found but are all fixed
+
+ ![validator](assets/images/validator.png)
+
+
+## Deployment
+
+This project was deployed using Code Institute mock terminal on Heroku
+
+### Steps for deployment
+* 
+* 
+* 
+* 
+# Credits
+Code Institute for deployment terminal
+
--- run.py
+++ run.py
@@ -1,22 +1,40 @@
 import random
-
 # creating the global variables
 # the player sets the numerical value of the board size
 # this then sets the size of the playable board size to be displayed
-board_size = 6
-# player board displays were the player positioned thier ships and updated information miss and hits 
-player_board = []
-# only the pseudo board is displayed on screen showing only updated miss and hits 
+# player board displays were the player positioned thier ships
+# and updated information miss and hits
+# only the computer pseudo board is displayed on screen
+# showing only updated miss and hits
 # the com board has the ships locations
+# these dictionaries will hold the positions of the ships location on the board
+player_board = []
 com_board = []
 com_pseudo_board = []
-# in this basic version of battleships each ship equals a life 
-# when all 4 lives/ships are gone its game over
-player_lives = 4
-com_lives = 4
-# these dictionaries will hold the positions of the ships location on the board
-player_ships = {'battleship': [], 'destroyer': [], 'submarine': [], 'patrol boat': []}
-com_ships = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol boat': 0}
+player_ships = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol': 0}
+com_ships = {'battleship': 0, 'destroyer': 0, 'submarine': 0, 'patrol': 0}
+
+print('\n\n     ******----WELCOME TO BATTLESHIPS----******\n')
+print(' in this version of terminal output battleships game\n')
+print('             the boat icons = @ symbol')
+print('             the miss icons = O symbol')
+print('              the hit icons = X symbol')
+
+board_size = 0
+board_set = True
+while board_set:
+    print('\n   What board size would you like to play on?')
+    print("\n   Select a board size: A number between 5-14")
+    count = int(input())
+    if count in range(5, 15):
+        print('\n---"Good choice Captain, setting up radar systems now"---')
+        count += 1
+        board_set = False
+    else:
+        print("\nsorry that is an incorrect value")
+        print("you need to pick a number between 5-14")
+board_size = count
+
 
 def random_num():
     '''
@@ -27,25 +45,13 @@ def random_num():
         num = round((random.random()) * 10, )
         if num in range(0, 6):
             in_range = False
-
     return num
 
-def get_board_size():
 
-    board_set = True
-    while board_set:
-        print("\nSelect a board size: A number between 5-14")
-        count = int(input())
-        if count in range(5, 15):
-            print("good choice")
-            count += 1 
-            board_set = False
-        else:
-            print("\nsorry that is an incorrect value")
-            print("you need to pick a number between 5-14")
-    return count
-
-def set_board_size():     
+def set_board_size():
+    '''
+    function takes input from the player to position thir boats on the board
+    '''
     for x in range(board_size):
         player_board.append([])
         com_board.append([])
@@ -54,19 +60,20 @@ def set_board_size():
             player_board[x].append([' - '])
             com_board[x].append([' - '])
             com_pseudo_board[x].append([' - '])
+
+
 def set_ships():
-    #this is the conditional so that ships do not occupy the same space
     print("Please select the locations of your ships")
-    
     last_boat = []
     for key, value in player_ships.items():
         ship = key
         selection = True
         while selection:
-            print("\n" + f"select {ship} position")
+            print("\n" + f'select "{ship}" position' + "\n")
             column = board_size
             column -= 1
             print(f"please select a column from 0 to {column}")
+            # this conditional checks that ships do not occupy the same space
             col_pick = True
             while col_pick:
                 col = int(input())
@@ -75,7 +82,7 @@ def set_ships():
                 else:
                     col_pick = False
             rows = board_size
-            rows -= 1        
+            rows -= 1
             print(f"please select a row from 0 to {rows}")
             row_pick = True
             while row_pick:
@@ -88,7 +95,7 @@ def set_ships():
             check = 0
             for x in range(len(last_boat)):
                 if boat == last_boat[x]:
-                    check += 1 
+                    check += 1
             if check > 0:
                 print('\n---"This position is taken sailor"---\n')
             else:
@@ -109,13 +116,16 @@ def set_ships():
         com_board[row][col] = [' @ ']
     print('---"computer setting its ships"---')
     display_ships()
-    
+
+
 def display_ships():
+    '''
+    optional display of the player and computers boards
+    '''
     print("")
     print("Display boards")
     blue = True
     while blue:
-        
         print("To display players board select 'p'")
         print("To display computers board select 'c'")
         print("To skip board display select 's'")
@@ -125,13 +135,11 @@ def display_ships():
             print("   Player Board")
             print("  ", end=" ")
             for x in range(board_size):
-                
                 print('-----', end="-")
             print("")
             print("")
             print("  ", end=" ")
             for x in range(board_size):
-                
                 print(f'  {x}', end="   ")
             print("")
             print("")
@@ -141,19 +149,16 @@ def display_ships():
                     print(f'{player_board[x][y][0]}', end=" | ")
                 print("")
                 print("")
-
         elif selection == 'c':
             print("\n")
             print("   Computers Board")
             print("  ", end=" ")
             for x in range(board_size):
-                
                 print('-----', end="-")
             print("")
             print("")
             print("  ", end=" ")
             for x in range(board_size):
-                
                 print(f'  {x}', end="   ")
             print("")
             print("")
@@ -169,7 +174,11 @@ def display_ships():
         else:
             print("\n---That selection is not supported---\n")
 
+
 def start_game():
+    '''
+    function for the game itself
+    '''
     player_lives = 4
     com_lives = 4
     game_on = True
@@ -229,10 +238,8 @@ def start_game():
             elif player_board[com_guess1][com_guess2][0] == ' O ':
                 print('\n---"Ha, there just hitting open water and fish"---')
                 print('---"poor fish"---')
-
             elif player_board[com_guess1][com_guess2][0] == ' X ':
                 print('\n---"those idiots were aiming at a downed ship"---')
-                
             else:
                 print('\n---"SPLASH"---')
                 print('---"Thats a miss Captain"---\n')
@@ -252,12 +259,13 @@ def start_game():
 
 
 def main():
-    board_size = get_board_size()
+    '''
+    this is the main function for the whole game
+    '''
     set_board_size()
     display_ships()
     set_ships()
     start_game()
-    
+
+
 main()
-
-
